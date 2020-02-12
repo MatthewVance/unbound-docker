b9601829ceed1cf0baa3e3f08acb7d331a077c14
rYR79435 <60985157+rYR79435@users.noreply.github.com>
2020-02-12 21:15:40 +0100

    Include /dev/null for logging

--- 1.9.6/unbound.sh
+++ 1.9.6/unbound.sh
@@ -363,7 +363,7 @@ EOT
 fi
 
 mkdir -p /opt/unbound/etc/unbound/dev && \
-cp -a /dev/random /dev/urandom /opt/unbound/etc/unbound/dev/
+cp -a /dev/random /dev/urandom /dev/null /opt/unbound/etc/unbound/dev/
 
 mkdir -p -m 700 /opt/unbound/etc/unbound/var && \
 chown _unbound:_unbound /opt/unbound/etc/unbound/var && \
